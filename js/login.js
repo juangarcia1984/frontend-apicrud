d2f3dc629f9f9da222eb20d9e3049256182e70b0
juan <juanfergp00@gmail.com>
2025-03-20 16:10:01 -0500

    se crea login.js

---
+++ js/login.js
@@ -0,0 +1,47 @@
+//declarar variables globales formulario login
+userinput = document.querySelector("#usuarioform");
+passinput = document.querySelector("#contraform");
+btnlogin = document.querySelector(".btnlogin");
+
+//evento al boton del formulario
+btnlogin.addEventListener("click", ()=>{
+    let dataform = getdata(); 
+    senddata(dataform);
+});
+
+//funcion para validar el formulario
+//obtener datos del formulario
+let getdata = ()=>{
+    //validar formulario
+    let user;
+    if (userinput.value && passinput.value){
+        user = {
+            usuario: userinput.value,
+            contrasena: passinput.value
+        }
+        userinput.value = "";
+        passinput.value = "";
+    }else{
+        alert("INGRESE LOS DATOS CORRECTAMENTEr");
+    }
+    return user;
+ };
+
+ //funcion para recibir los datos y realizar la peticion al servidor
+ let senddata = async(data)=>{
+    let url = "http://localhost/backend-apiCrud/login";
+    try {
+        let respuesta = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        let userlogin = await respuesta.json();
+        console.log(userlogin)      
+    } catch (error) {
+        console.log(error);  
+    }
+ };
+      
